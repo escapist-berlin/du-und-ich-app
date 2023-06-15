4cd000ae92b168ce0032dd803cec89cefd05bf0f
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-06-16 00:06:22 +0200

    [WIP] AppFooter

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,16 @@
   <div>
     <router-view name="home" />
     <router-view name="about" />
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-
+import AppFooter from '@/components/AppFooter.vue';
 export default {
   name: "App",
+  components: {
+    AppFooter,
+  },
 }
 </script>
---
+++ src/components/AppFooter.vue
@@ -0,0 +1,53 @@
+<template>
+  <footer>
+    <nav>
+      <ul class="footer-links">
+        <li><a href="#">Alle Anzeigen</a></li>
+        <li><a href="#">1919-1933</a></li>
+        <li><a href="#">1950's</a></li>
+        <li><a href="#">1970's</a></li>
+        <li><a href="#">1980's</a></li>
+        <li><a href="#">1990's</a></li>
+        <li><a href="#">Schlagwörter</a></li>
+        <li><a href="#">Über Dieses Projekt</a></li>
+      </ul>
+    </nav>
+    <button>Click Me</button>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'AppFooter',
+};
+</script>
+
+<style lang="scss" scoped>
+  footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    border-top: 0.5px solid #261824;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .footer-links {
+    display: flex;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+
+    li {
+      margin-right: 10px;
+
+      a {
+        text-decoration: none;
+      }
+    }
+  }
+</style>
