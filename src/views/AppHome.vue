8ef5a00ccf748735c1dc2763a1b62bdb1b2e26b7
Denis Kolokolov <dennis.kolokolov@gmail.com>
2023-06-15 18:28:45 +0200

    [ADD] Vuetify

--- src/views/AppHome.vue
+++ src/views/AppHome.vue
@@ -1,7 +1,17 @@
 
 <template>
   <h1>This is a home page</h1>
+  <v-card-title>This is a home page</v-card-title>
+  <v-card-subtitle>This is a home page</v-card-subtitle>
+
 </template>
 
-  <script>
-  </script>
+<script>
+  export default {
+    data () {
+      return {
+
+      }
+    },
+  }
+</script>
